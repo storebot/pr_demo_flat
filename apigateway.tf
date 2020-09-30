50fe9884db283efa01d6a7099b3ed101747392fa
Accurics <admin@accurics.com>
2020-09-30 00:25:52 +0000

    Auto Remediation By Accurics

--- apigateway.tf
+++ apigateway.tf
@@ -2,4 +2,9 @@ resource "aws_api_gateway_stage" "apiGatewayLogging" {
   stage_name    = "prod"
   rest_api_id   = "some-id"
   deployment_id = "some-id"
+
+  access_log_settings {
+    destination_arn = "<destination_arn>"
+    format          = "<format>>"
+  }
 }
