51fc4ba85e8cabd52084272da85dcf83e7f7c1f2
Accurics <admin@accurics.com>
2020-09-30 00:31:02 +0000

    Auto Remediation By Accurics

--- ebsvolume.tf
+++ ebsvolume.tf
@@ -1,6 +1,7 @@
 resource "aws_ebs_volume" "ebsVolumeNotEncrypted" {
   availability_zone = "us-west-2a"
   size              = 40
+  encrypted         = true
 }
 
 resource "aws_ebs_volume" "ebsVolumeEncryptedWithNoKms" {
