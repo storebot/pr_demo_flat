988dcb1d3bfa92779b8bb06277d9e4f3c6f03058
Accurics <admin@accurics.com>
2020-09-30 00:31:22 +0000

    Auto Remediation By Accurics

--- kinesisfirehose.tf
+++ kinesisfirehose.tf
@@ -14,4 +14,8 @@ resource "aws_kinesis_firehose_delivery_stream" "kinesisSseEnabled" {
 resource "aws_kinesis_firehose_delivery_stream" "kinesisSseNotConfigured" {
   name        = "terraform-kinesis-firehose-test-stream"
   destination = "s3"
+
+  server_side_encryption {
+    enabled = true
+  }
 }
