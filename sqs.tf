41a4d9abb824cb1325308ee084305893ff6b1de7
Accurics <admin@accurics.com>
2020-10-05 19:32:06 +0000

    Auto Remediation By Accurics

--- sqs.tf
+++ sqs.tf
@@ -6,16 +6,5 @@ resource "aws_sqs_queue" "sqsQueueExposed" {
   name                              = "terraform-example-queue"
   kms_master_key_id                 = "alias/aws/sqs"
   kms_data_key_reuse_period_seconds = 300
-  policy                            = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-    "Sid":"Queue1_AnonymousAccess_AllActions_WhitelistIP",
-    "Effect": "Allow",
-    "Principal": "*",
-    "Action": "sqs:*",
-    "Resource": "arn:aws:sqs:*:111122223333:queue1"
-  }] 
-}
-EOF
+  policy                            = "{\"Statement\":[{\"Action\":\"sqs:*\",\"Effect\":\"Allow\",\"Principal\":\"##principal###\",\"Resource\":\"arn:aws:sqs:*:111122223333:queue1\",\"Sid\":\"Queue1_AnonymousAccess_AllActions_WhitelistIP\"}],\"Version\":\"2012-10-17\"}"
 }
