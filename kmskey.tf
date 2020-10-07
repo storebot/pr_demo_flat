db07c024e945e5d713e235069367d6c965b134a6
Accurics <admin@accurics.com>
2020-10-07 16:44:46 +0000

    Auto Remediation By Accurics

--- kmskey.tf
+++ kmskey.tf
@@ -1,12 +1,12 @@
 resource "aws_kms_key" "kmsKeyDisabled" {
   description = "KMS key 2"
-  is_enabled  = false
+  is_enabled  = true
 }
 
 resource "aws_kms_key" "kmsKeyRotationDisabled" {
   description         = "KMS key 2"
   is_enabled          = true
-  enable_key_rotation = false
+  enable_key_rotation = true
 }
 
 resource "aws_kms_key" "kmsKeyNoDeletionWindow" {
@@ -18,18 +18,5 @@ resource "aws_kms_key" "kmsKeyNoDeletionWindow" {
 
 resource "aws_kms_key" "kmsKeyExposedPolicy" {
   description = "KMS key 2"
-  policy      = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "kms:*"
-      ],
-      "Principal": { "AWS": "*" },
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+  policy      = "{\"Statement\":[{\"Action\":[\"kms:##kms_action##\"],\"Principal\":\"##principal##\",\"Resource\":\"*\"}],\"Version\":\"2012-10-17\"}"
 }
