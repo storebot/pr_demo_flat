92348e1de85f800143abc7080674f6f93b8ca8c6
Accurics <admin@accurics.com>
2020-10-07 16:44:58 +0000

    Auto Remediation By Accurics

--- iamgrouppolicy.tf
+++ iamgrouppolicy.tf
@@ -1,18 +1,5 @@
 resource "aws_iam_group_policy" "iamGrpPolicyWithFullAdminCntrl" {
   name   = "test"
   group  = "test"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+  policy = "{\"Statement\":[{\"Action\":[\"##resource:action##\"],\"Effect\":\"Allow\",\"Resource\":\"*\"}],\"Version\":\"2012-10-17\"}"
 }
