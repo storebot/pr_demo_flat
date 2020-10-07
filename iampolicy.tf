35cbeb825ce1979535a8c60e0fd030ebfac5901e
Accurics <admin@accurics.com>
2020-10-07 16:06:09 +0000

    Auto Remediation By Accurics

--- iampolicy.tf
+++ iampolicy.tf
@@ -1,18 +1,5 @@
 resource "aws_iam_policy" "iamPolicyWithFullAdminControl" {
   name = "test"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+  policy = "{\"Statement\":[{\"Action\":[\"##resource:action##\"],\"Effect\":\"Allow\",\"Resource\":\"*\"}],\"Version\":\"2012-10-17\"}"
 }
