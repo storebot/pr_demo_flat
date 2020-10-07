97cf55ead002bb922d028c7d9c03609714124878
Accurics <admin@accurics.com>
2020-10-07 16:45:11 +0000

    Auto Remediation By Accurics

--- s3.tf
+++ s3.tf
@@ -6,6 +6,10 @@ resource "aws_s3_bucket" "noS3BucketSseRules" {
     Name        = "nos3BucketSseRules7"
     Environment = "Dev"
   }
+
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "s3BucketSseRulesWithKmsNull" {
@@ -24,11 +28,15 @@ resource "aws_s3_bucket" "s3BucketSseRulesWithKmsNull" {
       }
     }
   }
+
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "s3BucketNoWebsiteIndexDoc" {
   bucket = "website"
-  acl    = "public-read"
+  acl    = "private"
 
   server_side_encryption_configuration {
     rule {
@@ -42,6 +50,10 @@ resource "aws_s3_bucket" "s3BucketNoWebsiteIndexDoc" {
   website {
     index_document = "index.html"
     error_document = "error.html"
+  }
+
+  versioning {
+    enabled = true
   }
 }
 
@@ -58,29 +70,41 @@ resource "aws_s3_bucket" "s3VersioningMfaFalse" {
   }
 
   versioning {
-    enabled    = false
-    mfa_delete = false
+    enabled    = true
+    mfa_delete = true
   }
 }
 
 resource "aws_s3_bucket" "allUsersReadAccess" {
   bucket = "my-tf-test-bucket"
-  acl    = "public-read"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "authUsersReadAccess" {
   bucket = "my-tf-test-bucket"
-  acl    = "authenticated-read"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "allUsersWriteAccess" {
   bucket = "my-tf-test-bucket"
-  acl    = "public-read-write"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "allUsersReadWriteAccess" {
   bucket = "my-tf-test-bucket"
-  acl    = "public-read-write"
+  acl    = "private"
 
   server_side_encryption_configuration {
     rule {
@@ -89,22 +113,277 @@ resource "aws_s3_bucket" "allUsersReadWriteAccess" {
       }
     }
   }
+
+  versioning {
+    enabled = true
+  }
 }
 
 resource "aws_s3_bucket" "s3userVersioningMFAOnly" {
   bucket = "s3user-versioning-mfa-only"
-  acl    = "public-read-write"
+  acl    = "private"
   versioning {
-    enabled = false
+    enabled = true
   }
 }
 
 resource "aws_s3_bucket" "s3userVersioningOnly" {
   bucket = "s3user-versioning-mfa-only"
-  acl    = "public-read-write"
+  acl    = "private"
   versioning {
     mfa_delete = true
   }
 }
 
 
+
+resource "aws_s3_bucket_policy" "allUsersReadWriteAccessPolicy" {
+  bucket = "${aws_s3_bucket.allUsersReadWriteAccess.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "allUsersReadWriteAccess-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.allUsersReadWriteAccess.id}/*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_s3_bucket_policy" "s3userVersioningOnlyPolicy" {
+  bucket = "${aws_s3_bucket.s3userVersioningOnly.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "s3userVersioningOnly-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.s3userVersioningOnly.id}/*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_s3_bucket_policy" "s3userVersioningMFAOnlyPolicy" {
+  bucket = "${aws_s3_bucket.s3userVersioningMFAOnly.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "s3userVersioningMFAOnly-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.s3userVersioningMFAOnly.id}/*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_s3_bucket_policy" "allUsersWriteAccessPolicy" {
+  bucket = "${aws_s3_bucket.allUsersWriteAccess.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "allUsersWriteAccess-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.allUsersWriteAccess.id}/*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_s3_bucket_policy" "noS3BucketSseRulesPolicy" {
+  bucket = "${aws_s3_bucket.noS3BucketSseRules.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "noS3BucketSseRules-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.noS3BucketSseRules.id}/*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_s3_bucket_policy" "s3VersioningMfaFalsePolicy" {
+  bucket = "${aws_s3_bucket.s3VersioningMfaFalse.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "s3VersioningMfaFalse-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.s3VersioningMfaFalse.id}/*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_s3_bucket_policy" "authUsersReadAccessPolicy" {
+  bucket = "${aws_s3_bucket.authUsersReadAccess.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "authUsersReadAccess-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.authUsersReadAccess.id}/*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_s3_bucket_policy" "allUsersReadAccessPolicy" {
+  bucket = "${aws_s3_bucket.allUsersReadAccess.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "allUsersReadAccess-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.allUsersReadAccess.id}/*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_s3_bucket_policy" "s3BucketSseRulesWithKmsNullPolicy" {
+  bucket = "${aws_s3_bucket.s3BucketSseRulesWithKmsNull.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "s3BucketSseRulesWithKmsNull-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.s3BucketSseRulesWithKmsNull.id}/*"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_s3_bucket_policy" "s3BucketNoWebsiteIndexDocPolicy" {
+  bucket = "${aws_s3_bucket.s3BucketNoWebsiteIndexDoc.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "s3BucketNoWebsiteIndexDoc-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "arn:aws:iam::##acount_id##:role/##role_name##",
+            "arn:aws:iam::##acount_id##:user/##user_name##"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.s3BucketNoWebsiteIndexDoc.id}/*"
+    }
+  ]
+}
+POLICY
+}
