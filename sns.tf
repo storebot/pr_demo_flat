30411ed38e00bdd0ff3a05bb5ddebd48df2cbc57
Accurics <admin@accurics.com>
2020-10-07 16:06:11 +0000

    Auto Remediation By Accurics

--- sns.tf
+++ sns.tf
@@ -1,17 +1,4 @@
 resource "aws_sns_topic" "snsPublicAccess" {
   name   = "terraform-example-queue"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [{
-    "Sid":"Queue1_AnonymousAccess_AllActions_WhitelistIP",
-    "Effect": "Allow",
-    "Principal": { "AWS":"*" },
-    "Action": [
-      "SNS:*"
-    ],
-    "Resource": "arn:aws:sns:us-east-2:123456789012:MyTopic"
-  }] 
-}
-EOF
+  policy = "{\"Statement\":[{\"Action\":[\"SNS:*\"],\"Effect\":\"Allow\",\"Principal\":{\"AWS\":\"##principal##\"},\"Resource\":\"arn:aws:sns:us-east-2:123456789012:MyTopic\",\"Sid\":\"Queue1_AnonymousAccess_AllActions_WhitelistIP\"}],\"Version\":\"2012-10-17\"}"
 }
