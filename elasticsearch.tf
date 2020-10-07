254a8ebc7e1d5de3d919f2c7b6e13c0c8fe64d2e
Accurics <admin@accurics.com>
2020-10-07 16:45:16 +0000

    Auto Remediation By Accurics

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -5,7 +5,7 @@ resource "aws_elasticsearch_domain" "esloggingdisabled" {
   log_publishing_options {
     cloudwatch_log_group_arn = "arn:aws:logs:us-west-2:641885301384:log-group:siac_log_group_example:*"
     log_type                 = "INDEX_SLOW_LOGS"
-    enabled                  = false
+    enabled                  = true
   }
 
   tags = {
