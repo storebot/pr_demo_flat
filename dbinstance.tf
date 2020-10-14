d9852ee12d1a04b0f357651c7d1f213d8553b4ca
Accurics <admin@accurics.com>
2020-10-14 22:33:58 +0000

    Auto Remediation By Accurics

--- dbinstance.tf
+++ dbinstance.tf
@@ -1,11 +1,12 @@
 resource "aws_db_instance" "rdsBackupDisbaled" {
-  allocated_storage       = 20
-  storage_type            = "gp2"
-  engine                  = "mysql"
-  engine_version          = "5.7"
-  instance_class          = "db.t2.micro"
-  name                    = "mydb"
-  backup_retention_period = 0
+  allocated_storage                   = 20
+  storage_type                        = "gp2"
+  engine                              = "mysql"
+  engine_version                      = "5.7"
+  instance_class                      = "db.t2.micro"
+  name                                = "mydb"
+  backup_retention_period             = 30
+  iam_database_authentication_enabled = true
 }
 
 resource "aws_db_instance" "rdsIamAuthDisabled" {
@@ -16,5 +17,5 @@ resource "aws_db_instance" "rdsIamAuthDisabled" {
   instance_class                      = "db.t2.micro"
   name                                = "mydb"
   backup_retention_period             = 90
-  iam_database_authentication_enabled = false
+  iam_database_authentication_enabled = true
 }
